42e750d2a556acc23c356d8d89e38e0b61483d04
stinalaurell <stina.laurell@hyperisland.se>
2021-10-11 14:21:39 +0200

    Jquery + ScrollMagic script

--- script.js
+++ script.js
@@ -33,3 +33,43 @@ setTime()
 
 // Update the count down every 1 second
 var x = setInterval(setTime, 1000);
+
+
+
+
+
+
+//SCROLL DOWN PANELS (MAJORITY PANELS)
+
+/*
+
+if ($( window ).width() >= 1024) {
+
+    $(function () { // wait for document ready
+      //init
+      const controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+          triggerHook: 'onLeave',
+          duration: "100%" // this works just fine with duration 0 as well
+          // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+          // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+        }
+      });
+    
+      // get all slides
+      const slides = document.querySelectorAll("section.section");
+    
+      // create scene for every slide
+      for (var i = 0; i < slides.length; i++) {
+        new ScrollMagic.Scene({
+          triggerElement: slides[i]
+        })
+          .setPin(slides[i], { pushFollowers: false })
+          //.addIndicators() // add indicators (requires plugin)
+          .addTo(controller);
+      }
+      
+    });
+    }
+    
+    */
