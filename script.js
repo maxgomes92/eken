4fc495b490f61e68a85ba0acc378642dccdf6a3f
stinalaurell <stina.laurell@hyperisland.se>
2021-10-13 16:20:24 +0200

    New responsive nav bar for desktop+phone+tablet

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
 //CLOCK
 
 // Set the date we're counting down to
