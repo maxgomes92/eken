3fe4084b625f25a83e43d059b17ad430120934fa
stinalaurell <stina.laurell@hyperisland.se>
2021-10-14 21:15:11 +0200

    Q&A + Beräkningsmodell created, mobile & iPad

---
+++ q&a.js
@@ -0,0 +1,107 @@
+function myFunction1() {
+    var x = document.getElementById("hide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("hide2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("hide3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("hide4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction5() {
+    var x = document.getElementById("hide5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction6() {
+    var x = document.getElementById("hide6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction7() {
+    var x = document.getElementById("hide7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction8() {
+    var x = document.getElementById("hide8");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction9() {
+    var x = document.getElementById("hide9");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction10() {
+    var x = document.getElementById("hide10");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction11() {
+    var x = document.getElementById("hide11");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction12() {
+    var x = document.getElementById("hide12");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
