f5097ee9b82641a5c0a085bb1e369946aa8135fa
stinalaurell <stina.laurell@hyperisland.se>
2021-10-25 19:42:53 +0200

    New -pixels for hiding top nav on scroll

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("myTopnav").style.top = "0";
   } else {
-    document.getElementById("myTopnav").style.top = "-50px";
+    document.getElementById("myTopnav").style.top = "-52px";
   }
   prevScrollpos = currentScrollPos;
 }
