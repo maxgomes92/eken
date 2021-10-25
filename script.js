a358688d87ee3f69634259c2ceddd2428b66600f
stinalaurell <stina.laurell@hyperisland.se>
2021-10-25 19:22:31 +0200

    New hide-nav-bar

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ function togglePageLinks() {
   }
 }
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("myTopnav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 
 //CLOCK
 
