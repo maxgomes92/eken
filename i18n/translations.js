c0b4387130fc6b4805a06b6df433c7481ede9222
stinalaurell <stina.laurell@hyperisland.se>
2021-10-30 17:30:29 +0200

    Translation, index-page done, except for pictures

--- i18n/translations.js
+++ i18n/translations.js
@@ -16,16 +16,44 @@ const i18n = {
         title: '{years} Years, {days} days, <br> {hours} hours, {minutes} minutes <br> and {seconds} seconds',
       },
       section_two: {
-        title: 'Det är ohållbart att leva ohållbart i <span style="color: #3D1107">10 år</span> till',
+        title: 'Living like there is no<span style="color: #3D1107"> tomorrow</span>, might just mean that..'
       },
       section_three: {
-        title: 'Vi behöver stora och effektiva klimatlösningar.<br><br>Nu.',
+        title: 'We need climate solutions with impact.<br><br>Today.',
       },
       section_four: {
         title: 'One of Sweden\'s most ambitious climate initiatives',
+        text: 'We are launching a first\-time opportunity to invest in carbon storage in mature Swedish forests, in trees that are contracted to grow ten years longer than planned felling.  The climate impact is immediate and extensive. Our goal is to join forces with forest owners and companies to store twice the carbon dioxide emissions in the Nordic region; 500 million tonnes of CO2e.',
       },
       section_five: {
-        title: 'Världens största och mest effektiva kollager är bara halvfullt',
+        title: 'The world\'s largest and most efficient carbon storage is only half full',
+        text: 'Forest is a hyper-efficient and natural vacuum cleaner of carbon dioxide, but from a climate perspective trees are harvested too early. If they grow longer before felling, the volume of timber in the forests can double and store twice as much carbon.'
+      },
+      section_six: {
+        title: 'Young forest emits carbon dioxide, adult forests are the most efficient carbon storages in the world',
+        text: 'A growing forest absorbs carbon through the photosynthesis, but also emits carbon dioxide through trees and soil. Emissions occur when trees and fungi die, or during combustion. When trees have grown large enough, trunks, branches, twigs, leaves, fungi and roots absorb more carbon dioxide than they emit, and forest then becomes an extremely efficient and natural storage for carbon.'
+      },
+      section_seven: {
+        title: 'The climate crisis is now, and so must the solution',
+        text: 'EKEN FINANCING contracts trees that are registered for clear-cutting to grow for at least ten more years, to enable a carbon storage offer in mature Swedish forests. The large amount of carbon dioxide that would have been released into the atmosphere after clear-cutting remains in the trees, and they continue to grow and store even more carbon dioxide. Today, this is the most immediate and impactful climate action.'
+      },
+      section_eight: {
+        title: 'Real climate impact. For companies that want to make a real difference.',
+        text: 'We are all part of the global climate crisis through greenhouse gas emissions. Companies urgently need to heavily reduce their emissions an move towards climate neutrality. But since time is short, they also need to invest in projects that immediately and powerfully reduce carbon dioxide in the atmosphere. Climate compensation is not instead of, it is in addition to.<br><br>Our product EKEN STOP10 is an opportunity for companies to reduce their climate footprint credibly and transparently through climate compensation with immediate impact. As an example, the effect will be just over 1,000 CO2e when a normal-sized Swedish felling of 2.3 hectares is postponed for ten years. And since more than 200,000 hectares of Swedish forest are felled every year, it is possible to store much more carbon than we do today.',
+        text2: 'Our solution makes the climate effect measurable and transparent down to individual trees. And most importantly, the impact is immediate. 75 percent already within five years.'
+      },
+      section_nine: {
+        title: 'eken stop10',
+        list: '<li>○ Powerful climate effect now – 75% within five years. </li><br><li>○ Natural and local - carbon storage in mature Swedish forests.</li><br><li>○ Measurability and transparency - certificate with property designation, geographical position, effect calculation and image documentation.</li><br><li>○ Annual reporting of the climate effect down to individual trees.</li><br><li>○ Participation in one of Sweden\'s most ambitious climate projects - storage of 2x carbon dioxide emissions in the Nordic region.</li><br>'
+      },
+      section_ten: {
+        title: 'Green light for forest owners to let the trees grow for another ten years',
+        text: 'Our offer EKEN PLUS10 gives forest owners an economical opportunity to let trees grow. We also support research and development that enables forest owners to shift to a profitable and climate-smart forestry as soon as possible. If they want to.'
+      },
+      footer: {
+        text:'EKEN FINANCING was founded in the meeting between the commercial forest industry and eco-based forestry. Our starting point has been 25 years of data from climate-smart forestry, established science and new technology.',
+        tel:'Contact: +4670-2603682',
+        mail:'<a href="mailto:info@ekenfinancing.com">info@ekenfinancing.com</a>'
       },
     },
   },
@@ -53,9 +81,37 @@ const i18n = {
       },
       section_four: {
         title: 'Ett av Sveriges mest ambitiösa klimatinitiativ',
+        text: 'Vi lanserar nu som första företag en möjlighet att investera i koldioxidlagring i befintlig svensk skog. I träd som får växa tio år längre än planerad avverkning. Den positiva klimateffekten är omedelbar och stor - vårt mål är att tillsammans med skogsägare och företag lagra dubbelt så mycket som koldioxidutsläppen i Norden; 500 miljoner ton CO2e.',
       },
       section_five: {
         title: 'Världens största och mest effektiva kollager är bara halvfullt',
+        text: 'Skog är en hypereffektiv och naturlig dammsugare av koldioxid, men ur ett klimatperspektiv avverkar vi för tidigt. Om träd för växa längre innan avverkning kan virkesvolym och koldioxidlagring fördubblas.'
+      },
+      section_six: {
+        title: 'Ungskog släpper ut koldioxid, vuxen skog är världens effektivaste kollager',
+        text: 'Skog tar upp kol via fotosyntesen, men släpper samtidigt ut koldioxid genom träd och mark. Utsläpp sker när träd och svampar dör, eller vid förbränning. När träd har vuxit sig tillräckligt stora tar stammar, grenar, kvistar, löv, svampar och rötter upp mer koldioxid än de släpper ut, och skog blir då till en oerhört effektiv och naturlig lagringsplats för kol.'
+      },
+      section_seven: {
+        title: 'Klimatkrisen är nu, därför behöver också lösningen vara nu',
+        text: 'EKEN FINANCING avtalar om att låta avverkningsanmälda träd växa i ytterligare minst tio år, och kan då erbjuda koldioxidlagring i befintlig svensk skog. Den stora mängd koldioxid som skulle ha släppts ut i atmosfären vid kalavverkning stannar kvar i träden, som dessutom växer och lagrar ännu mer koldioxid. Idag är det här den mest omedelbara och effektiva lösningen för att kraftfullt reducera koldioxiden i atmosfären.'
+      },
+      section_eight: {
+        title: 'Klimateffekt på riktigt. För företag som vill göra skillnad, på riktigt.',
+        text: 'Vi är alla delaktiga i världens stora klimatkris genom utsläpp av växthusgaser. Företagen behöver akut kraftigt minska sina utsläpp och gå mot klimatneutralitet, men eftersom tiden är knapp behöver de samtidigt investera i projekt som ger snabb och stor klimateffekt. Klimatkompensation är alltså inte istället för, det är tillsammans med.<br><br>Vår produkt EKEN STOP10 är en möjlighet för företag att trovärdigt och transparent minska sitt klimatavtryck och få omedelbar och stor klimateffekt på sin investering. Som ett exempel blir effekten drygt 1000 CO2e då en normalstor svensk avverkning på 2,3 hektar flyttas fram i tio år. Och eftersom ungefär 200 000 hektar svensk skog avverkas varje år är det möjligt att lagra väldigt mycket mer koldioxid än vad vi gör idag.',
+        text2: 'Vår finansiella lösning gör klimateffekten mätbar och transparent ner till individuella träd. Och det viktigaste av allt, effekten kommer snabbt. 75 procent redan inom fem år.',
+      },
+      section_nine: {
+        title: 'eken stop10',
+        list: '<li>○ Stor klimateffekt nu - 75% inom fem år. </li><br><li>○ Naturligt och lokalt – koldioxidlagring i befintlig svensk skog i tio år.</li><br><li>○ Mätbarhet och transparens – certifikat med fastighetsbeteckning, geografisk position, effektberäkning och bilddokumentation.</li><br><li>○ Årsrapportering av klimateffekt ner till individuella träd.</li><br><li>○ Medverkan i ett av Sveriges mest ambitiösa klimatinitiativ – lagring av 2x koldioxidutsläppen i Norden.</li><br>'
+      },
+      section_ten: {
+        title: 'Nu är det grönt för skogsägare att låta träden växa tio år till',
+        text: 'Vårt erbjudande EKEN PLUS10 ger skogsägare en ekonomisk möjlighet att låta träd växa tio år till. Vi stöttar även forskning och utveckling som underlättar för skogsägare att på lång sikt kunna ställa om till framtidens lönsamma och klimatsmarta skogsbruk.'
+      },
+      footer: {
+        text:'EKEN FINANCING startades i mötet mellan kompetenser från den kommersiella skogsindustrin och ekobaserat skogsbruk. Vår utgångspunkt har varit 25 års data från klimatsmart skogsbruk, etablerad vetenskap och ny teknologi.',
+        tel:'Telefon: 070-2603682',
+        mail:'E-post: <a href="mailto:info@ekenfinancing.com">info@ekenfinancing.com</a>'
       },
     },
   },
