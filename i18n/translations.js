b6d02285eab0ad3e3cc5581aab2ed15cdd781894
stinalaurell <stina.laurell@hyperisland.se>
2021-10-30 16:22:55 +0200

    Trying out translations

--- i18n/translations.js
+++ i18n/translations.js
@@ -3,24 +3,54 @@ const i18n = {
     navbar: {
       section_five: 'HALF FULL FOREST',
       section_six: 'CARBON STORAGE',
-      language: "SWEDISH",
+      section_seven: 'BIG CLIMATE IMPACT',
+      section_eight: 'OFFER TO COMPANY',
+      section_ten: 'COLLAB WITH FORREST OWNERS',
+      section_q_a: 'Q & A',
+      section_calc: 'CALCULATION MODEL',
+      section_about: 'ABOUT US',
+      language: "SVENSKA",
     },
+
     page_index: {
       section_one: {
-        title: '{years} Years {days} days, <br> {hours} timmar, {minutes} minuter <br> och {seconds} sekunder'
-      }
-    }
+        title: '{years} Years, {days} days, <br> {hours} hours, {minutes} minutes <br> and {seconds} seconds',
+      },
+      section_four: {
+        title: 'One of Sweden´s most ambitious climate initiatives',
+      },
+    },
+
+
+
   },
+
   sv: {
     navbar: {
       section_five: 'SKOGEN ÄR HALVFULL',
       section_six: 'KOLDIOXIDLAGRING SKOG',
+      section_seven: 'STOR KLIMATEFFEKT NU',
+      section_eight: 'ERBJUDANDE FÖRETAG',
+      section_ten: 'SAMARBETE SKOGSÄGARE',
+      section_q_a: 'FRÅGOR OCH SVAR',
+      section_calc: 'BERÄKNINGSMODELL',
+      section_about: 'OM OSS',
       language: "ENGLISH",
     },
+
     page_index: {
       section_one: {
-        title: '{years} År {days} dagar, <br> {hours} timmar, {minutes} minuter <br> och {seconds} sekunder'
-      }
-    }
-  }
+        title: '{years} År {days} dagar, <br> {hours} timmar, {minutes} minuter <br> och {seconds} sekunder',
+      },
+      section_four: {
+        title: 'Ett av Sveriges mest ambitiösa klimatinitiativ',
+      },
+    },
+
+
+  },
+
+
+
 }
+
